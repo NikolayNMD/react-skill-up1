18b26ca35291b2ef4713f929c655a48b7e012688
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2025-03-25 20:57:59 +0200

    done first task mb

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,6 +1,10 @@
-function Task () {
+function Task (props) {
     return (
-        <></>
+        <div className="bg-cyan-100 p-10 rounded">
+            <h3 className="font-bold mb-5">{props.title}</h3>
+            <p>{props.description}</p>
+            <p className={props.isCompleted ? "text-green-500" : "text-red-500"}>{props.isCompleted ? "Завершено" : "Не завершено"}</p>
+        </div>
     );
 }
 
